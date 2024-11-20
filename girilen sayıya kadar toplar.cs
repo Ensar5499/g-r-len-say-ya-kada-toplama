fd7ebd4e2b4b12a63634708530af7c6d388e353f
Ensar5499 <ensarkulaksiz54@gmail.com>
2024-11-20 15:52:15 +0300

    Add files via upload
    Girilen sayıya kadar olan sayıları toplar.

---
+++ girilen sayıya kadar toplar.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ödeev
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            {
+               
+                Console.Write("Bir sayı girin: ");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+
+                int toplam = 0;
+
+               
+                for (int i = 1; i <= sayi; i++)
+                {
+                    toplam += i;  
+                }
+
+                
+                Console.WriteLine("Girilen sayıya kadar olan sayıların toplamı: " + toplam);
+            }
+                Console.ReadLine();
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
+
